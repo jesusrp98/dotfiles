5ef18f20e042763aa62fb72ded3926f3422bda30
LINUX-XPS <jesusrope98@outlook.com>
2019-02-11 10:00:02 +0100

    Fixed i3 init sh

--- i3/init.sh
+++ i3/init.sh
@@ -2,7 +2,6 @@
 
 # Specific LINUX PC stuff
 if [ "$(hostname)" == "LINUX-PC" ]; then
-    sudo colctl -m solidall -c 255,255,255 -c0 233,30,99 -c1 233,30,99 -c2 233,30,99 -c3 233,30,99 -c4 233,30,99 -c5 233,30,99 -c6 233,30,99 -c7 233,30,99 -ps 65
     xrandr --output HDMI-1 --mode 1920x1080 --pos 0x184 --rotate normal --output DP-3 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1 --off
     i3-msg 'workspace 1; append_layout ~/.dotfiles/i3/workspace1.json'
     i3-msg 'workspace 2; append_layout ~/.dotfiles/i3/workspace2.json'
