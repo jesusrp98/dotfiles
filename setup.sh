5e4da8011bd832fb67b765367c2e52adf786c05d
LINUX-PC <jesusrope98@outlook.com>
2019-04-18 13:23:12 +0200

    Added gtk3-nocsd package

--- setup.sh
+++ setup.sh
@@ -40,7 +40,7 @@ echo -e "\tPacman packages installed!"
 
 # Install packages from AUR
 echo -e "\n=======INSTALL AUR PACKAGES=======\n"
-pacaur -S --noconfirm --noedit polybar spotify oh-my-zsh-git android-studio
+pacaur -S --noconfirm --noedit polybar spotify oh-my-zsh-git android-studio gtk3-nocsd-git
 echo -e "\tAUR packages installed!"
 
 # Change default shell
