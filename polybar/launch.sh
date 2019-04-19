fe35c5379f4da81c0a3a14a577859e38451ec2d1
LINUX-PC <jesusrope98@outlook.com>
2019-04-19 12:31:22 +0200

    Tweaked polybar launch script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,7 +3,7 @@
 # Launches the specific polybar, depending on the device
 killall -q polybar
 if [ "$(hostname)" == "LINUX-PC" ]; then
-    polybar -r pc -c ~/.dotfiles/polybar/config &
+    polybar -r pc -c ~/.dotfiles/polybar/config&
 else
-    polybar -r xps -c ~/.dotfiles/polybar/config &
+    polybar -r xps -c ~/.dotfiles/polybar/config&
 fi
